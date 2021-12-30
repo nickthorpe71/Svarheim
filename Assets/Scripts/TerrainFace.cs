beb7e183e3999bfdc787f04833f00639521b278b
Nick Thorpe <nickthorpe71@gmail.com>
2021-12-30 10:32:59 -0600

    add ConstructMesh method

--- Assets/Scripts/TerrainFace.cs
+++ Assets/Scripts/TerrainFace.cs
@@ -18,10 +18,9 @@ public class TerrainFace
         axisA = Vector3.Cross(localUp, axisA);
     }
 
+    // there are 6 meshes (aka terrain faces) per sphere (aka planet)
     public void ConstructMesh()
     {
-        // there are 6 meshes (aka terrain faces) per sphere (aka planet)
-
         // since resolution is the number of vertices along the edge of a mesh
         // the total vertices in a mesh is resolution ^ 2
         Vector3[] vertices = new Vector3[resolution * resolution];
@@ -31,7 +30,33 @@ public class TerrainFace
         // multiplied by 2 triangles per square
         // multiplied by 3 vertices per triangle
         int[] triangles = new int[(resolution - 1) * (resolution - 1) * 2 * 3];
+        int triIndex = 0;
 
+        for (int y = 0; y < resolution; y++)
+            for (int x = 0; x < resolution; x++)
+            {
+                int i = x + y * resolution;
+                Vector2 percent = new Vector2(x, y) / (resolution - 1);
+                Vector3 pointOnUnitCube = localUp + (percent.x - 0.5f) * 2 * axisA + (percent.y - 0.5f) * 2 * axisB;
+                vertices[i] = pointOnUnitCube;
 
+                if (x != resolution - 1 && y != resolution - 1)
+                {
+                    triangles[triIndex] = i;
+                    triangles[triIndex + 1] = i + resolution + 1;
+                    triangles[triIndex + 2] = i + resolution;
+
+                    triangles[triIndex + 3] = i;
+                    triangles[triIndex + 4] = i + 1;
+                    triangles[triIndex + 5] = i + resolution + 1;
+
+                    triIndex += 6;
+                }
+            }
+
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
     }
 }
