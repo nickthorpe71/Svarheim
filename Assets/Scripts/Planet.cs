1adcf671318399b6cc99e2d1f5d3b0f5ae45cf57
Nick Thorpe <nickthorpe71@gmail.com>
2021-12-30 10:44:54 -0600

    add mesh generation methods to planet script

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -1,18 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Range(2, 256)]
+    public int resolution = 10;
+
+    [SerializeField, HideInInspector] MeshFilter[] meshFilters;
+    TerrainFace[] terrainFaces;
+
+    // update when something changes in the editor
+    private void OnValidate()
     {
-        
+        Initialize();
+        GenerateMesh();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Initialize()
     {
-        
+        if (meshFilters == null || meshFilters.Length == 0)
+            meshFilters = new MeshFilter[6];
+
+        terrainFaces = new TerrainFace[6];
+
+        // array of cardinal directions
+        Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+
+        for (int i = 0; i < 6; i++)
+        {
+            if (meshFilters[i] == null)
+            {
+                GameObject meshObj = new GameObject("mesh");
+                meshObj.transform.parent = transform;
+
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+                meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+                meshFilters[i].sharedMesh = new Mesh();
+            }
+
+            terrainFaces[i] = new TerrainFace(meshFilters[i].sharedMesh, resolution, directions[i]);
+        }
+    }
+
+    void GenerateMesh()
+    {
+        foreach (TerrainFace face in terrainFaces)
+            face.ConstructMesh();
     }
 }
