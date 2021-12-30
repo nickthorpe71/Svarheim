a115b9785890bfd1caf1040e3c72c47daefd1431
Nick Thorpe <nickthorpe71@gmail.com>
2021-12-30 13:46:40 -0600

    add noise filter and editor settings for planet noise

---
+++ Assets/Scripts/NoiseFilter.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class NoiseFilter
+{
+    NoiseSettings settings;
+    Noise noise = new Noise();
+
+    public NoiseFilter(NoiseSettings settings)
+    {
+        this.settings = settings;
+    }
+
+    public float Evaluate(Vector3 point)
+    {
+        float noiseValue = (noise.Evaluate(point * settings.roughness + settings.center) + 1) * 0.5f;
+        return noiseValue * settings.strength;
+    }
+}
---
+++ Assets/Scripts/NoiseSettings.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class NoiseSettings
+{
+    public float strength = 1;
+    public float roughness = 1;
+    public Vector3 center;
+}
