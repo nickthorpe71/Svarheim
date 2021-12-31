6c784c29e5cb158091e834caff34b3f158c9bd28
Nick Thorpe <nickthorpe71@gmail.com>
2021-12-30 21:20:15 -0600

    add layered noise to planet mesh

--- Assets/Scripts/ShapeGenerator.cs
+++ Assets/Scripts/ShapeGenerator.cs
@@ -2,17 +2,27 @@ using UnityEngine;
 public class ShapeGenerator
 {
     ShapeSettings settings;
-    NoiseFilter noiseFilter;
+    NoiseFilter[] noiseFilters;
 
     public ShapeGenerator(ShapeSettings settings)
     {
         this.settings = settings;
-        noiseFilter = new NoiseFilter(settings.noiseSettings);
+        noiseFilters = new NoiseFilter[settings.noiseLayers.Length];
+        for (int i = 0; i < noiseFilters.Length; i++)
+            noiseFilters[i] = new NoiseFilter(settings.noiseLayers[i].noiseSettings);
     }
 
     public Vector3 CalculatePointOnPlanet(Vector3 pointOnUnitSphere)
     {
-        float elevation = noiseFilter.Evaluate(pointOnUnitSphere);
+        float firstLayerValue = (noiseFilters.Length > 0) ? noiseFilters[0].Evaluate(pointOnUnitSphere) : 0;
+        float elevation = 0;
+
+        for (int i = 0; i < noiseFilters.Length; i++)
+        {
+            float mask = (settings.noiseLayers[i].useFirstLayerAsMask) ? firstLayerValue : 1;
+            elevation += (settings.noiseLayers[i].enabled) ? noiseFilters[i].Evaluate(pointOnUnitSphere) : 0;
+        }
+
         return pointOnUnitSphere * settings.planetRadius * (1 + elevation);
     }
 }
