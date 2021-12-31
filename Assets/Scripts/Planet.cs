20c29acdeabe0b26a0f2637467edc918056d236e
Nick Thorpe <nickthorpe71@gmail.com>
2021-12-31 10:15:09 -0600

    add planet gradient shader

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    [Header("Settings")]
     [Range(2, 256)]
     public int resolution = 10;
     public bool autoUpdate = true;
@@ -12,21 +11,28 @@ public class Planet : MonoBehaviour
     public ShapeSettings shapeSettings;
     public ColorSettings colorSettings;
 
-    [HideInInspector] public bool shapeSettingsFoldout;
-    [HideInInspector] public bool colorSettingsFoldout;
+    [HideInInspector]
+    public bool shapeSettingsFoldout;
+    [HideInInspector]
+    public bool colorSettingsFoldout;
 
-    ShapeGenerator shapeGenerator;
+    ShapeGenerator shapeGenerator = new ShapeGenerator();
+    ColorGenerator colorGenerator = new ColorGenerator();
 
     [SerializeField, HideInInspector]
     MeshFilter[] meshFilters;
     TerrainFace[] terrainFaces;
 
+
     void Initialize()
     {
-        shapeGenerator = new ShapeGenerator(shapeSettings);
-        if (meshFilters == null || meshFilters.Length == 0)
-            meshFilters = new MeshFilter[6];
+        shapeGenerator.UpdateSettings(shapeSettings);
+        colorGenerator.UpdateSettings(colorSettings);
 
+        if (meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[6];
+        }
         terrainFaces = new TerrainFace[6];
 
         Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
@@ -38,10 +44,11 @@ public class Planet : MonoBehaviour
                 GameObject meshObj = new GameObject("mesh");
                 meshObj.transform.parent = transform;
 
-                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+                meshObj.AddComponent<MeshRenderer>();
                 meshFilters[i] = meshObj.AddComponent<MeshFilter>();
                 meshFilters[i].sharedMesh = new Mesh();
             }
+            meshFilters[i].GetComponent<MeshRenderer>().sharedMaterial = colorSettings.planetMaterial;
 
             terrainFaces[i] = new TerrainFace(shapeGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);
             bool renderFace = faceRenderMask == FaceRenderMask.All || (int)faceRenderMask - 1 == i;
@@ -58,28 +65,37 @@ public class Planet : MonoBehaviour
 
     public void OnShapeSettingsUpdated()
     {
-        if (!autoUpdate) return;
-        Initialize();
-        GenerateMesh();
+        if (autoUpdate)
+        {
+            Initialize();
+            GenerateMesh();
+        }
     }
 
     public void OnColorSettingsUpdated()
     {
-        if (!autoUpdate) return;
-        Initialize();
-        GenerateColors();
+        if (autoUpdate)
+        {
+            Initialize();
+            GenerateColors();
+        }
     }
 
     void GenerateMesh()
     {
         for (int i = 0; i < 6; i++)
+        {
             if (meshFilters[i].gameObject.activeSelf)
+            {
                 terrainFaces[i].ConstructMesh();
+            }
+        }
+
+        colorGenerator.UpdateElevation(shapeGenerator.elevationMinMax);
     }
 
     void GenerateColors()
     {
-        foreach (MeshFilter m in meshFilters)
-            m.GetComponent<MeshRenderer>().sharedMaterial.color = colorSettings.planetColor;
+        colorGenerator.UpdateColors();
     }
 }
--- Assets/Scripts/ShapeGenerator.cs
+++ Assets/Scripts/ShapeGenerator.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class ShapeGenerator
 {
+
     ShapeSettings settings;
     INoiseFilter[] noiseFilters;
+    public MinMax elevationMinMax;
 
-    public ShapeGenerator(ShapeSettings settings)
+    public void UpdateSettings(ShapeSettings settings)
     {
         this.settings = settings;
         noiseFilters = new INoiseFilter[settings.noiseLayers.Length];
@@ -13,6 +15,7 @@ public class ShapeGenerator
         {
             noiseFilters[i] = NoiseFilterFactory.CreateNoiseFilter(settings.noiseLayers[i].noiseSettings);
         }
+        elevationMinMax = new MinMax();
     }
 
     public Vector3 CalculatePointOnPlanet(Vector3 pointOnUnitSphere)
@@ -37,6 +40,8 @@ public class ShapeGenerator
                 elevation += noiseFilters[i].Evaluate(pointOnUnitSphere) * mask;
             }
         }
-        return pointOnUnitSphere * settings.planetRadius * (1 + elevation);
+        elevation = settings.planetRadius * (1 + elevation);
+        elevationMinMax.AddValue(elevation);
+        return pointOnUnitSphere * elevation;
     }
 }
